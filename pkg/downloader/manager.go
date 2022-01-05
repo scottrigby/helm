f616a01808da3428c6191e5196f32ca72eb22254
Scott Rigby <scott@r6by.com>
2022-01-05 18:46:57 -0500

    It appears we never got to this block below. Quick rec by Farina. Untested if necessary
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- pkg/downloader/manager.go
+++ pkg/downloader/manager.go
@@ -578,8 +578,7 @@ func (m *Manager) resolveRepoNames(deps []*chart.Dependency) (map[string]string,
 	missing := []string{}
 	for _, dd := range deps {
 		// Don't map the repository, we don't need to download chart from charts directory
-		// When OCI is used there is no Helm repository
-		if dd.Repository == "" || registry.IsOCI(dd.Repository) {
+		if dd.Repository == "" {
 			continue
 		}
 		// if dep chart is from local path, verify the path is valid
@@ -595,6 +594,8 @@ func (m *Manager) resolveRepoNames(deps []*chart.Dependency) (map[string]string,
 			continue
 		}
 
+		// See https://helm.sh/docs/topics/registries/#specifying-dependencies
+		// See createTestingMetadataForOCI()
 		if registry.IsOCI(dd.Repository) {
 			reposMap[dd.Name] = dd.Repository
 			continue
