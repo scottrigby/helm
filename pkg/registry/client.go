23dcd75c438fb05722280831c7c700efcb272888
Scott Rigby <scott@r6by.com>
2025-06-01 18:29:31 -0400

    OCI default to OAuth2
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- pkg/registry/client.go
+++ pkg/registry/client.go
@@ -153,6 +153,9 @@ func NewClient(options ...ClientOption) (*Client, error) {
 			authorizer.Cache = auth.NewCache()
 		}
 
+		// default to OAuth flow
+		authorizer.ForceAttemptOAuth2 = true
+
 		client.authorizer = &authorizer
 	}
 
