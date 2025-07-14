eb8fc4aba309a8ba3994a8bd7f5cde6ccb29b198
Scott Rigby <scott@r6by.com>
2025-07-14 12:57:50 -0400

    Note this old todo for moving postrenderer to plugin system is now complete:
    	// NOTE(thomastaylor312): I am leaving this code commented out here. During
    	// the implementation of post-render, it was brought up that if we are
    	// relying on plugins, we should actually use the plugin system so it can
    	// properly handle multiple OSs. This will be a feature add in the future,
    	// so I left this code for reference. It can be deleted or reused once the
    	// feature is implemented
    
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- pkg/postrender/exec.go
+++ pkg/postrender/exec.go
@@ -24,7 +24,6 @@ import (
 	"io"
 	"os"
 	"os/exec"
-	"path/filepath"
 )
 
 // TODO this should be a plugin name instead of binary path
@@ -94,45 +93,4 @@ func (p *execRender) Run(renderedManifests *bytes.Buffer) (*bytes.Buffer, error)
 	}
 
 	return postRendered, nil
-}
-
-// getFullPath returns the full filepath to the binary to execute. If the path
-// does not contain any separators, it will search in $PATH, otherwise it will
-// resolve any relative paths to a fully qualified path
-func getFullPath(binaryPath string) (string, error) {
-	// NOTE(thomastaylor312): I am leaving this code commented out here. During
-	// the implementation of post-render, it was brought up that if we are
-	// relying on plugins, we should actually use the plugin system so it can
-	// properly handle multiple OSs. This will be a feature add in the future,
-	// so I left this code for reference. It can be deleted or reused once the
-	// feature is implemented
-
-	// Manually check the plugin dir first
-	// if !strings.Contains(binaryPath, string(filepath.Separator)) {
-	// 	// First check the plugin dir
-	// 	pluginDir := helmpath.DataPath("plugins") // Default location
-	// 	// If location for plugins is explicitly set, check there
-	// 	if v, ok := os.LookupEnv("HELM_PLUGINS"); ok {
-	// 		pluginDir = v
-	// 	}
-	// 	// The plugins variable can actually contain multiple paths, so loop through those
-	// 	for _, p := range filepath.SplitList(pluginDir) {
-	// 		_, err := os.Stat(filepath.Join(p, binaryPath))
-	// 		if err != nil && !errors.Is(err, fs.ErrNotExist) {
-	// 			return "", err
-	// 		} else if err == nil {
-	// 			binaryPath = filepath.Join(p, binaryPath)
-	// 			break
-	// 		}
-	// 	}
-	// }
-
-	// Now check for the binary using the given path or check if it exists in
-	// the path and is executable
-	checkedPath, err := exec.LookPath(binaryPath)
-	if err != nil {
-		return "", fmt.Errorf("unable to find binary at %s: %w", binaryPath, err)
-	}
-
-	return filepath.Abs(checkedPath)
 }
