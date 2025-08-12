5bbfbd70c78b6211a30c7010a6094a64acd07228
George Jenkins <gvjenkins@gmail.com>
2025-08-12 09:21:58 -0700

    basic tests

--- pkg/plugin/runtime_extismv1.go
+++ pkg/plugin/runtime_extismv1.go
@@ -1,3 +1,18 @@
+/*
+Copyright The Helm Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package plugin
 
 import (
@@ -12,13 +27,6 @@ import (
 	"helm.sh/helm/v4/pkg/plugin/schema"
 	"sigs.k8s.io/yaml"
 )
-
-type RuntimeExtismV1 struct {
-	HostFunctions    map[string]extism.HostFunction
-	CompliationCache wazero.CompilationCache
-}
-
-var _ Runtime = (*RuntimeExtismV1)(nil)
 
 type RuntimeConfigExtismV1 struct {
 	MaxPages             uint32            `json:"maxPages,omitempty"`
@@ -38,6 +46,13 @@ func (r *RuntimeConfigExtismV1) Validate() error {
 	// TODO
 	return nil
 }
+
+type RuntimeExtismV1 struct {
+	HostFunctions    map[string]extism.HostFunction
+	CompliationCache wazero.CompilationCache
+}
+
+var _ Runtime = (*RuntimeExtismV1)(nil)
 
 func (r *RuntimeExtismV1) CreatePlugin(pluginDir string, metadata *Metadata) (Plugin, error) {
 
